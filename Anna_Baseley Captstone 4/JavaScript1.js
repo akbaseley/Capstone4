b5d84e1078f7f6962db5ef104fd328fba98e64cf
Anna B <akbaseley@gmail.com>
2018-05-22 15:21:46 -0400

    First commit of Capstone 4

---
+++ Anna_Baseley Captstone 4/JavaScript1.js
@@ -0,0 +1,23 @@
+﻿
+var optionList = [];
+var priceList = [];
+function add(name, price)
+{
+
+    optionList.push(name);
+    priceList.push(price);
+
+    document.getElementById('ShoppingCart').innerHTML += `<tr><td>${name}</td><td>$${price.toFixed(2)}</td></tr>`;
+    addTotal();
+}
+
+function addTotal()
+{
+    total = 0;
+    for(var i in priceList)
+    {
+        total =+ priceList[i];
+    }
+    document.getElementById('CartTotal').innerHTML = `<tr><td>${"Cart Total: "}</td><td>$${total.toFixed(2)}</td></tr>`;
+
+}
